7ee2fd95bd642a8fc29f423a19dd0853fa8e7d24
Salil Bavdekar <salil91@gmail.com>
2021-05-13 14:13:56 -0400

    Added electronegatives for CN=1

--- gasp/electronegativities.py
+++ gasp/electronegativities.py
@@ -13,23 +13,23 @@ def get_EN(Z, CN):
     # Look up electronegativities
     #df_EN = pd.read_excel("EN_lookup.xlsx", header = None, names = range(1,13))
     #df_EN.index = np.arange(1, len(df_EN)+1)
-    
+
     EN_dict = {
     1: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     2: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     3: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     4: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    5: {1: float('nan'),  2: 1.641,  3: 1.641,  4: 1.641,  5: float('nan'),  6: 1.415,  7: 1.415,  8: float('nan'),  9: 1.415,  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    6: {1: float('nan'),  2: 2.619,  3: 2.619,  4: 2.5,  5: float('nan'),  6: 2.347,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    7: {1: float('nan'),  2: float('nan'),  3: 3.565,  4: 3.437,  5: float('nan'),  6: 3.085,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    8: {1: float('nan'),  2: float('nan'),  3: 4.561,  4: 4.375,  5: float('nan'),  6: 3.75,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    5: {1: 1.641),  2: 1.641,  3: 1.641,  4: 1.641,  5: float('nan'),  6: 1.415,  7: 1.415,  8: float('nan'),  9: 1.415,  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    6: {1: 2.619,  2: 2.619,  3: 2.619,  4: 2.5,  5: float('nan'),  6: 2.347,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    7: {1: 3.565,  2: float('nan'),  3: 3.565,  4: 3.437,  5: float('nan'),  6: 3.085,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    8: {1: 4.561,  2: float('nan'),  3: 4.561,  4: 4.375,  5: float('nan'),  6: 3.75,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     9: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     10: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     11: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     12: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     13: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    14: {1: float('nan'),  2: 1.645,  3: 1.645,  4: 1.645,  5: float('nan'),  6: 1.565,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    15: {1: float('nan'),  2: float('nan'),  3: 2.187,  4: 2.187,  5: float('nan'),  6: 2.074,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    14: {1: 1.645,  2: 1.645,  3: 1.645,  4: 1.645,  5: float('nan'),  6: 1.565,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    15: {1: 2.187,  2: float('nan'),  3: 2.187,  4: 2.187,  5: float('nan'),  6: 2.074,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     16: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     17: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     18: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
@@ -38,7 +38,7 @@ def get_EN(Z, CN):
     21: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     22: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     23: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    24: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: 0.659,  8: float('nan'),  9: 0.659,  10: float('nan'),  11: float('nan'),  12: 0.659},
+    24: {1: 0.659,  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: 0.659,  8: float('nan'),  9: 0.659,  10: float('nan'),  11: float('nan'),  12: 0.659},
     25: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     26: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     27: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
@@ -47,7 +47,7 @@ def get_EN(Z, CN):
     30: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     31: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     32: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    33: {1: float('nan'),  2: float('nan'),  3: 2.039,  4: 2.039,  5: float('nan'),  6: 1.925,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    33: {1: 2.039,  2: float('nan'),  3: 2.039,  4: 2.039,  5: float('nan'),  6: 1.925,  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     34: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     35: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     36: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
@@ -56,7 +56,7 @@ def get_EN(Z, CN):
     39: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     40: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     41: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    42: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: 2.171,  11: float('nan'),  12: float('nan')},
+    42: {1: 2.171,  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: 2.171,  11: float('nan'),  12: float('nan')},
     43: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     44: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     45: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
@@ -88,9 +88,9 @@ def get_EN(Z, CN):
     71: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     72: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
     73: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    74: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: 2.171,  11: float('nan'),  12: float('nan')},
-    75: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: 0.71,  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
-    76: {1: float('nan'),  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: 1.504,  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')}
+    74: {1: 2.171,  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: float('nan'),  9: float('nan'),  10: 2.171,  11: float('nan'),  12: float('nan')},
+    75: {1: 0.71,  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: 0.71,  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')},
+    76: {1: 1.504,  2: float('nan'),  3: float('nan'),  4: float('nan'),  5: float('nan'),  6: float('nan'),  7: float('nan'),  8: 1.504,  9: float('nan'),  10: float('nan'),  11: float('nan'),  12: float('nan')}
     }
 
     return EN_dict[Z][CN]
